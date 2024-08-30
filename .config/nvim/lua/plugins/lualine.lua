325f310d0a3c56f9055a31a719ab1dc7860a52f2
Arthur Khachaturov <me@wzray.ru>
2024-08-30 15:21:22 +0300

    editing dotfiles day 15

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,15 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+        options = {
+            component_separators = { left = '', right = '' },
+            section_separators = { left = '', right = '' },
+            disabled_filetypes = {
+                'NvimTree',
+                'qf',
+                'trouble',
+            },
+        }
+    }
+}
